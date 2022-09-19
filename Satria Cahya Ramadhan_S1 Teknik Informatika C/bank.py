3e8a93d8479956be1a2ac5b30ffba3e7ebfbe213
satria <estetok69@gmail.com>
2022-09-19 20:11:45 +0700

    first commit

---
+++ Satria Cahya Ramadhan_S1 Teknik Informatika C/bank.py
@@ -0,0 +1,9 @@
+answer = input("hi? ").strip().lower()
+if answer == "hello" or answer == "hello, newman":
+    print("0$")
+elif answer == "how you doing?": 
+    print("20$")
+elif answer == "what's happening":
+    print("100$")
+else:
+    print("ya ndak tau kok tanya saya")
